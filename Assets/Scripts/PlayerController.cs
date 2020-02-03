dfc517e8b8dd30cfce29a361ef8afeafb470341f
Philtherage <phillipwood61@gmail.com>
2020-02-03 09:47:32 -0500

    Serialized the clamps in the inspector

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,13 @@ public class PlayerController : MonoBehaviour
     [Tooltip("In meters per second")]
     [SerializeField] float xSpeed = 10f;
     [SerializeField] float xRotateSpeed = 10f;
+    [SerializeField] float xScreenClamp = 5f;
+    [SerializeField] float xRotateClamp = 40f;
     [SerializeField] float ySpeed = 10f;
     [SerializeField] float yRotateSpeed = 10f;
+    [SerializeField] float yScreenClamp = 3.5f;
+    [SerializeField] float yRotateClamp = 40f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +34,7 @@ public class PlayerController : MonoBehaviour
         float xOffsetThisFrame = horizontalThrow * xSpeed * Time.deltaTime;
         float rawNewXPos = transform.localPosition.x + xOffsetThisFrame;
 
-        float xPos = Mathf.Clamp(rawNewXPos, -5, 5);
+        float xPos = Mathf.Clamp(rawNewXPos, -xScreenClamp, xScreenClamp);
         transform.localPosition = new Vector3(xPos, transform.localPosition.y, transform.localPosition.z);
         XRotation(horizontalThrow);
     }
@@ -37,7 +42,7 @@ public class PlayerController : MonoBehaviour
     private void XRotation(float horizontalThrow)
     {
         float rawRotatePos = horizontalThrow * xRotateSpeed * Time.deltaTime;
-        float rotateClamp = Mathf.Clamp(rawRotatePos, -40, 40);
+        float rotateClamp = Mathf.Clamp(rawRotatePos, -xRotateClamp, xRotateClamp);
         transform.localRotation = new Quaternion(transform.localRotation.x, transform.localRotation.y, -rotateClamp, transform.localRotation.w);
     }
 
@@ -47,15 +52,15 @@ public class PlayerController : MonoBehaviour
         float yOffsetThisFrame = VerticalThrow * ySpeed * Time.deltaTime;
         float rawNewYPos = transform.localPosition.y + yOffsetThisFrame;
 
-        float yPos = Mathf.Clamp(rawNewYPos, -3.5f, 3.5f);
+        float yPos = Mathf.Clamp(rawNewYPos, -yScreenClamp, yScreenClamp);
         transform.localPosition = new Vector3(transform.localPosition.x, yPos, transform.localPosition.z);
         YRotation(VerticalThrow);
     }
 
     private void YRotation(float verticalThrow)
     {
-        float rawRotatePos = verticalThrow * xRotateSpeed * Time.deltaTime;
-        float rotateClamp = Mathf.Clamp(rawRotatePos, -40, 40);
+        float rawRotatePos = verticalThrow * yRotateSpeed * Time.deltaTime;
+        float rotateClamp = Mathf.Clamp(rawRotatePos, -yRotateClamp, yRotateClamp);
         transform.localRotation = new Quaternion(-rotateClamp, transform.localRotation.y, transform.localRotation.z, transform.localRotation.w);
     }
 }
